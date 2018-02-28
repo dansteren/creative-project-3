32914eeb2bbf1d71d43020ef54a4cfc24ba45387
Dan Steren <daniel.d.steren@gmail.com>
2018-02-28 13:42:26 -0700

    Initial Commit

---
+++ src/components/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomePage from '@/components/HomePage'
+import Create from '@/components/Create'
+import Todo from '@/components/Todo'
+import NotFound from '@/components/NotFound'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '/create',
+      name: 'Create',
+      component: Create
+    },
+    {
+      path: '/todo',
+      name: 'Todo',
+      component: Todo
+    },
+    {
+      path: '*',
+      component: NotFound
+    }
+  ]
+})
